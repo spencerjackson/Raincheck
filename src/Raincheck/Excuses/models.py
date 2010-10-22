1397d88392104aa3b4f58beb59a0b556d5fe1df3
Lee <leeyuan123@yahoo.com>
2010-10-22 18:06:12 -0400

    Git ignore + corrected models

--- src/Raincheck/Excuses/models.py
+++ src/Raincheck/Excuses/models.py
@@ -1,3 +1,8 @@
+# Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Excuse(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User)
