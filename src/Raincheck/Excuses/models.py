1ff99de334cc17f01baa071ad21fe673a505dbce
Nohemi Fernandez <nf68@cornell.edu>
2010-10-22 19:44:32 -0400

    Add enum to excuses

--- src/Raincheck/Excuses/models.py
+++ src/Raincheck/Excuses/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Excuse(models.Model):
+    TYPE_OF_EXCUSE = (
+	('I', 'Illness')
+	('P', 'Personal')
+	('W', 'Work')
+	('F', 'Funny')
+    )
     text = models.TextField()
+    excuse = models.CharField(max_length=1, choices=TYPE_OF_EXCUSE)
     author = models.ForeignKey(User)
     like = models.IntegerField()
     dislike = models.IntegerField()
