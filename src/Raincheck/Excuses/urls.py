f2113abc2943f874ac86087070036c925c6418f2
Lee <leeyuan123@yahoo.com>
2010-10-23 10:19:13 -0400

    Added profile page and individual permalinks to excuses.

--- src/Raincheck/Excuses/urls.py
+++ src/Raincheck/Excuses/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('Raincheck.Excuses.views',
  (r'(?P<excuseID>\d+)/(?P<excuse_is_liked>\D+)/$', 'vote'),
+ (r'(?P<id>\d+)/?$', 'single'),
  (r'create','create')
 )
 
