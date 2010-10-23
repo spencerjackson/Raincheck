5f13b7f29793860dc4131acedfb9eb7bbad70598
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-10-22 22:37:07 -0400

    Fix missing paren

--- src/Raincheck/FacebookOAuth/urls.py
+++ src/Raincheck/FacebookOAuth/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',    
     (r'FBOAuth/$', 'Raincheck.FacebookOAuth.views.auth'),
     (r'FBOAuth/dance/?', 'Raincheck.FacebookOAuth.views.dance'),
-
+)
