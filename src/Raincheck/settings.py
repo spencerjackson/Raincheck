3a0b117706824499c8922c40cbcc6d4617b16d4d
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-10-23 00:47:37 -0400

    Add login page

--- src/Raincheck/settings.py
+++ src/Raincheck/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 )
 
 ROOT_URLCONF = 'Raincheck.urls'
